3a49a07f1ceeaa55ad0a61ecbaf3beb4e70e76a7
Norayr Chilingarian <norayr@arnet.am>
2019-09-06 10:52:53 +0400

    shell script fixed

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #replace awk or fmt part with 'lowercase' or 'makewords' program of your own to test
-#cat $1 | awk '{print tolower($0)}' | fmt -1 | uniq | sort| sed 's/\.//g' | comm -13 ../dictionary -
+cat text_to_spell | awk '{print tolower($0)}' | fmt -1 | uniq | sort| sed 's/\.//g' | comm -13 dictionary -
